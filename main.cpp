ee958140791c1f43caef07c3fabefd946530e27d
Ishan[KBG] <ishan.199512@gmail.com>
2021-01-17 10:56:27 +0000

    Added temperature.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
-#include <iostream>
+/* #include <iostream>
 
 int main() {
-  
+
   int tip = 0;
-  
+
   std::cout << "Enter tip amount: ";
   std::cin >> tip;
-  
+
   std::cout << "You paid " << tip << " dollars.\n";
-  
+
 }
+*/
